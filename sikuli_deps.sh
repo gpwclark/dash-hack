b418aacff816f1adae3fe38bc72946dbec6f1c0d
gpwclark <gpwclark@gmail.com>
2016-02-29 18:43:13 -0500

    Closes issue #6, oracle java 7 installs automatically now

--- sikuli_deps.sh
+++ sikuli_deps.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
 
+echo oracle-java7-installer shared/accepted-oracle-license-v1-1 select true | sudo /usr/bin/debconf-set-selections
 sudo add-apt-repository -y ppa:webupd8team/java
 sudo apt-get -y update
 sudo apt-get -y install oracle-java7-installer
